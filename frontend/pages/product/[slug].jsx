b80a581e01eeceec4562d5251e4572789c2c8123
huuquang12 <58297575+huuquang12@users.noreply.github.com>
2022-03-03 12:42:59 +0700

    Add to Cart function

--- frontend/pages/product/[slug].jsx
+++ frontend/pages/product/[slug].jsx
@@ -1,9 +1,3 @@
-import { useRouter } from "next/router";
-import React from "react";
-import Layout from "../../components/Layout";
-import data from "../../utils/data";
-import NextLink from "next/link";
-import Image from "next/image";
 import {
   Button,
   Card,
@@ -11,26 +5,36 @@ import {
   Link,
   List,
   ListItem,
-  Typography,
+  Typography
 } from "@material-ui/core";
+import axios from "axios";
+import Image from "next/image";
+import NextLink from "next/link";
+import React, { useContext } from "react";
+import Layout from "../../components/Layout";
+import { Store } from "../../utils/Store";
 import useStyles from "../../utils/styles";
+import Product from '../../models/Product';
+import db from '../../utils/db';
 
-export default function ProductScreen() {
-  const classes = useStyles();
+export default function ProductScreen(props) {
+  const { dispatch } = useContext(Store);
 
-  const router = useRouter();
-
-  const { slug } = router.query;
-
-  const product = data.products.find((a) => a.slug === slug);
-
-  const addToCartHandler = () => {
-    
-  }
-
+  const { product } = props;
   if (!product) {
     return <div>Product Not Found</div>;
   }
+
+  const classes = useStyles();
+
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert("Sorry. This product is out of stock");
+      return;
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity: 1 } });
+  };
 
   return (
     <Layout title={product.name} description={product.description}>
@@ -116,4 +120,18 @@ export default function ProductScreen() {
       </Grid>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
 }
---
+++ frontend/utils/Store.js
@@ -0,0 +1,37 @@
+import { createContext, useReducer } from "react";
+import Cookies from "js-cookie";
+
+export const Store = createContext();
+const initialState = {
+  cart: {
+    cartItems: Cookies.get("cartItems")
+      ? JSON.parse(Cookies.get("cartItems"))
+      : [],
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item._id === newItem._id
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.name === existItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      Cookies.set("cartItems", JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    default:
+      return state;
+  }
+}
+
+export function StoreProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+}
