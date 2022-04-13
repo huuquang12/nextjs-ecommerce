e9cc3eb8f7cb768464cdea94d978b46f9ccb8b3c
NguyenXuanDuan <nguyenxuanduanlqd@gmail.com>
2022-04-13 16:47:10 +0700

    update Google Map

--- frontend/components/mapbox-map.jsx
+++ frontend/components/mapbox-map.jsx
@@ -1,35 +1,48 @@
 import * as React from "react";
-import mapboxgl from "mapbox-gl";
+import mapboxgl, { Popup } from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-
+import Shipping from "../pages/shipping";
+import { useForm } from "react-hook-form";
+import  Googlemap from '../components/GoogleMap';
 
 
 function MapboxMap({
   initialOptions = {},
   onCreated,
+  handleClickMarker,
   onLoaded,
   onRemoved,
 }) {
   const [map, setMap] = React.useState();
   const [locationClick, setLocationClick] = React.useState([
-    105.85009963133263, 20.99773467489078,
+    105.800880, 20.994040,
   ]);
 
   const mapNode = React.useRef(null);
+  //creat popup
+  var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Thanh Xuân');
+  var popup1 = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Đống Đa');
+var popup2 = new mapboxgl.Popup({ offset: 25 }).setText(
+      'Cầu Giấy');
+var popup3 = new mapboxgl.Popup({ offset: 25 }).setText(
+        'Hà Đông');
+
 
   React.useEffect(() => {
     const node = mapNode.current;
 
     if (typeof window === "undefined" || node === null) return;
 
-    // Ha Noi - Viet Nam
+    // Thanh Xuan - Ha Noi
     const mapboxMap = new mapboxgl.Map({
       container: node,
       accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [105.85009963133263, 20.99773467489078],
+      center: [105.800880, 20.994040],
       zoom: 15,
       ...initialOptions,
     });
@@ -52,13 +65,45 @@ function MapboxMap({
         accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "",
       })
     );
-    //create marker
-    const marker1 = new mapboxgl.Marker({ color: "red" })
-      .setLngLat([105.85009963133263, 20.99773467489078])
+    //create marker Thanh Xuan
+    const marker = new mapboxgl.Marker({ color: "red" })
+      .setLngLat([105.800880, 20.994040])
+      .setPopup(popup)
       .addTo(mapboxMap);
-    mapboxMap.on("click", (e) =>
-      marker1.setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(mapboxMap)
-    );
+      const element = marker.getElement();
+    element.addEventListener("click", (e) => {
+      handleClickMarker && handleClickMarker("Thanh Xuân");
+    });
+    // mapboxMap.on("click", (e) =>
+    //   marker1.setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(mapboxMap)
+    // );
+    //Dong Da
+    const marker1 = new mapboxgl.Marker({ color: "black" })
+    .setLngLat([105.827618, 21.017536])
+    .setPopup(popup1)
+    .addTo(mapboxMap);
+    const element1 = marker1.getElement();
+    element1.addEventListener("click", (e) => {
+      handleClickMarker && handleClickMarker("Đống Đa");
+    });
+    //Cau Giay
+    const marker2 = new mapboxgl.Marker({ color: "green" })
+    .setLngLat([105.791677, 21.032448])
+    .setPopup(popup2)
+    .addTo(mapboxMap);
+    const element2 = marker2.getElement();
+    element2.addEventListener("click", (e) => {
+      handleClickMarker && handleClickMarker("Cầu Giấy");
+    });
+    //Ha Dong - VietNam
+    const marker3 = new mapboxgl.Marker({ color: "yellow" })
+    .setLngLat([105.768202, 20.973854])
+    .setPopup(popup3)
+    .addTo(mapboxMap);
+    const element3 = marker3.getElement();
+    element3.addEventListener("click", (e) => {
+      handleClickMarker && handleClickMarker("Hà Đông");
+    });
 
     setMap(mapboxMap);
     if (onCreated) onCreated(mapboxMap);
